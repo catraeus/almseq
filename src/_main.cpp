0b37a5a45a86d5f454e13d614570e62e1625fe6d
Duncan Gray <duncang@catraeus.com>
2022-04-16 09:54:28 -0500

    All Shiny and New

---
+++ src/_main.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : almseq.cpp
+// Author      : Catraeus & Duncan Gray
+// Version     :
+// Copyright   : Copyright © 2022
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+
+int main() {
+  int a;
+
+  a = 57;
+  fprintf(stdout, "Gotcha %d!\n", a); fflush(stdout);
+  return 0;
+}
